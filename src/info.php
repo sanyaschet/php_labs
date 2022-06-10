1a7aa9b4a45c7019002516f61ff7fe656359cf63
aschetson <shchetnev-a@google.com>
2022-06-10 20:35:21 +0300

    add info page

---
+++ src/info.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>info</title>
+ </head>
+ <body>
+ <?php
+  echo "<p>SERVER_NAME: $_SERVER[SERVER_NAME]\n</p>";
+  echo "<p>HTTP_REFERER: $_SERVER[HTTP_USER_AGENT]\n</p>";
+  echo "<p>HTTP_REFERER: $_SERVER[HTTP_REFERER]\n</p>";
+ ?>
+ </body>
+</html>
